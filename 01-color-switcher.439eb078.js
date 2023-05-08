3b66bbd2b72d42886893907180a27502883ed027
TiranoUser <103372475+TiranoUser@users.noreply.github.com>
2023-05-08 20:50:12 +0000

    Deploying to gh-pages from @ TiranoUser/goit-js-hw-09@1f2f3115146429da7653721efb3c96a22ee51835 🚀

--- 01-color-switcher.439eb078.js
+++
@@ -1,2 +0,0 @@
-startEl=document.querySelector("[data-start]"),stopEl=document.querySelector("[data-stop]");let t=null;startEl.addEventListener("click",(function(){t=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,startEl.disabled=!0}),1e3)})),stopEl.addEventListener("click",(()=>{clearInterval(t),startEl.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.439eb078.js.map
